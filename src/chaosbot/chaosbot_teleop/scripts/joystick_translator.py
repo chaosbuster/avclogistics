f15c1d97390d5c740e0609f3c5e727a60bf0e159
Jeanette Breton <jeanette@techno-chaos.com>
2018-07-26 23:02:29 -0600

    Added wrist and hand to joystick
    Wrist acts on Button 6(Close) and 8(Open), assumes Servo 7 (Base-One)
    Hand acts on Button 5(Close) and 7(Open), assumes Servo 8 (Base-One)

--- src/chaosbot/chaosbot_teleop/scripts/joystick_translator.py
+++ src/chaosbot/chaosbot_teleop/scripts/joystick_translator.py
@@ -31,39 +31,74 @@ from i2cpwm_board.msg import Servo, ServoArray
 # NOTE: Using MODE where the MODE button light is ON
 # axis 3 aka right stick vertical controls linear speed
 # axis 2 aka right stick horizontal controls angular speed
-
-# Speed directions of our chassis
-GOFORWARD=1
-NOSPEED=0
-GOREVERSE=-1
-ZERO_HIGH = 0.01
-ZERO_LOW = -0.01
-
     
 joystick = Twist()
-chaosbot_cmds = [ Servo() for i in range(4)]
+chaosbot_cmds = [ Servo() for i in range(6)]
+global pos_wrist, pos_hand
 
 def callback_allcurrentinputs(data):
     rospy.loginfo('Received a joystick input')
+    # Initialize the wrist and hand to neutral (middle) position
+    global pos_wrist, pos_hand
 
     joystick.linear.x = data.axes[3]
     joystick.angular.z = data.axes[2]
 
-    # Set the steering (negative's are right, positive's are left)
+    # Set the steering 
+    # Negative's are right, Positive's are left
     chaosbot_cmds[0].servo = 2
     chaosbot_cmds[0].value = data.axes[2]
 
-    # Set the speed for the chassis (negative's are reverse, positive's are forward)
+    # Set the speed for the chassis
+    # Negative's are reverse, Positive's are forward
     chaosbot_cmds[1].servo = 1
     chaosbot_cmds[1].value = data.axes[3]
 
-    # Set the speed for the shoulder (negative's are reverse, positive's are forward)
+    # Set the speed for the shoulder 
+    # Negative's are reverse towards the back
+    # Positive's are forward towards the front
     chaosbot_cmds[2].servo = 5
     chaosbot_cmds[2].value = data.axes[1]
 
-    # Set the speed for the shoulder (negative's are reverse, positive's are forward)
+    # Set the speed for the elbow
+    # Negative's are reverse towards the back
+    # Positive's are forward towards the front
     chaosbot_cmds[3].servo = 6
     chaosbot_cmds[3].value = data.axes[0]
+
+    # Set the position for the wrist
+    if data.buttons[7] <> 0:
+         # If Button 8 then Positive to open
+         # For safety doing open direction if both buttons
+         pos_wrist = pos_wrist + 0.1
+    elif data.buttons[5] <> 0:
+         # If Button 6 then Negative to close
+         pos_wrist = pos_wrist - 0.1
+    else:
+         # If No Button 6 or 8 then no change
+         pos_wrist = pos_wrist
+
+    # The wrist 
+    # Channel 6 Base-Zero
+    # Base-One for i2c software interface below
+    chaosbot_cmds[4].servo = 7
+    chaosbot_cmds[4].value = pos_wrist
+
+    if data.buttons[6] <> 0:
+    # If Button 7 then Positive to open
+         pos_hand = pos_hand + 0.1
+    elif data.buttons[4] <> 0:
+    # If Button 5 then Negative to close
+         pos_hand = pos_hand - 0.1
+    else:
+    # If No Button 6 or 8 then no change
+         pos_hand = pos_hand
+
+    # The hand 
+    # Channel 7 Base-Zero
+    # Channel 8 Base-One for i2c software interface below
+    chaosbot_cmds[5].servo = 8  
+    chaosbot_cmds[5].value = pos_hand
 
     #pubPlanned.publish(joystick)
     pubControl.publish(chaosbot_cmds)
@@ -72,9 +107,12 @@ def callback_allcurrentinputs(data):
 # Initializes everything
 def joystick_translator():
     global pubPlanned, pubControl
+    global pos_wrist, pos_hand
 
     # Starting the node
     rospy.init_node('joystick_translator')
+    pos_wrist = 0
+    pos_hand = 0
 
     # publishing a geometry cmd_vel Twist to control ChaosBot in Planning Simulations
     # pubPlanned = rospy.Publisher('cmd_vel', Twist, queue_size=10)
@@ -88,9 +126,9 @@ def joystick_translator():
 
 if __name__ == '__main__':
     try:
-        joystick_translator()
+         joystick_translator()
     except rospy.ROSInterruptException:
-        pass
+         pass
 
  
 
